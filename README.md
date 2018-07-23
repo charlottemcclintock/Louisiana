699b48d9d415976161e437af5db50c16fe4badec
Charlotte McClintock <ccm9yd@virginia.edu>
2018-07-23 13:52:06 -0600

    first organization commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# LouisianaGov
+
+
+# About
+#### A Project of RunforOffice & DataforDemocracy 
+
+Add a paragraph here on the aim of the project (i.e. The aim of this project is to explore diversity and inclusion at all levels of government, investigating representation across demographics including race, gender, and age, and it's impact on state and local politics across the United States. The initial focus is Louisiana, but the eventual goal is to expand the project to every state.)
+
+# Data Sources
+
+* Population parameters *from the Census Bureau*
+    +  Racial demographics of the state of Louisiana from the 2016 5-Year American Community Survey, retrieved through [SocialExplorer](https://www.socialexplorer.com/tables/ACS2016_5yr/R11751646).
+    +  Population of cities and towns retrieved from [American FactFinder](https://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml)
+* Race, party, gender, and position of elected officials *from WHERE?*
+    + 
+* Age and address of elected officials *from Louisiana Secretary of State Voter File*
+    + Retrieved from [the Louisiana Secretary of State](https://www.sos.la.gov/ElectionsAndVoting/BecomeACandidate/PurchaseVoterLists/Pages/default.aspx).
+  
+# Code & Scripts 
+
+To view the code for this project, click [here](https://www.github.com/charlottemcclintock/LouisianaGov).
+
+# Analysis & Visualizations
+
+To view the analysis, download the `doc.html` file. 
---
+++ viz.R
@@ -0,0 +1,295 @@
+
+## C. McClintock 
+## Data for Democracy - RunForOffice
+## Summer 2018 
+## Visualizations
+
+# ..................................................................................................
+
+# set up: wd, retrieve data
+rm(list=ls())
+getwd()
+# if need be 
+setwd("LouisianaGov/data")
+
+# set up: libraries
+library(dplyr)
+library(forcats)
+library(tidyverse)
+library(readxl)
+library(tidyr)
+library(scales)
+library(RColorBrewer)
+
+load("LA.Rdata")
+
+# ..................................................................................................
+
+# GENDER AND ELECTED OFFICE
+
+# GENDER BY LEVEL OF GOVERNMENT
+la.b1 <- ggplot(data = subset(la, !is.na(gender)), aes(x = level)) + 
+  geom_bar(aes(fill = gender), position = "fill") +
+  scale_fill_manual(values = c("#bf0549", "#059cbf")) +
+  labs(title = "Elected Offices by Gender of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Gender", x = "Level of Government", y = "Percent of Elected Officials") +
+  scale_y_continuous(labels = percent) + 
+  geom_hline(yintercept = .5, col="black", linetype=2) +
+  annotate("text", x = c(1,2,3), y = c(1.04, 1.04, 1.04), 
+           label = c("n = 4348","n = 224", "n = 8")) 
+
+xtabs(~la$level + la$gender)
+
+# GENDER BY TYPE OF POSITION
+la.b1a <- ggplot(data = subset(la, !is.na(gender)), aes(x = type)) + 
+  geom_bar(aes(fill = gender), position = "fill") +
+  scale_fill_manual(values = c("#bf0549", "#059cbf")) +
+  labs(title = "Elected Offices by Gender of Elected Officiaal", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Gender", x = "Type of Position", y = "Percent of Elected Officials") +
+  scale_y_continuous(labels = percent) + 
+  geom_hline(yintercept = .5, col="black", linetype=2) +
+  annotate("text", x = c(1,2,3,4,5,6), y = c(1.04, 1.04, 1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 651","n = 751", "n = 1848","n = 311","n = 152", "n = 867")) 
+
+xtabs(~la$type + la$gender)
+
+# ..................................................................................................
+
+# RACE AND ELECTED OFFICE
+
+# add population measures for comparison
+
+df <- full_join(la, pop, by = c("race", "level", "type"))
+df <- mutate(df,
+             level = as.factor(level),
+             level = fct_relevel(level,
+                              "2016 Louisiana Population", 
+                              "Local", 
+                              "State", 
+                              "Federal"))
+
+df <- mutate(df,
+             type = as.factor(type),
+             type = fct_relevel(type,
+                                 "2016 Louisiana Population", 
+                                 "Education",
+                                 "Law Enforcement",
+                                 "Local Government",
+                                 "Executive", 
+                                 "Legislative", 
+                                 "Judicial"))
+
+# RACE BY LEVEL OF GOVERNMENT
+la.b2 <- ggplot(data = subset(df, !is.na(race)), aes(x = level)) + 
+  geom_bar(aes(fill = race), position = "fill") +
+  scale_fill_manual(values = brewer.pal(9, "Blues")[c(8,6,4)]) +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Elected Offices by Race of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Race", x = "Level of Government", y = "Percent of Elected Officials")  + 
+  geom_hline(yintercept = .626, col="black", linetype=2) + 
+  geom_hline(yintercept = .678, col="black", linetype=2) +
+  annotate("text", x = c(1,2,3), y = c(1.04, 1.04, 1.04), 
+           label = c("n = 4348","n = 224", "n = 8")) 
+
+xtabs(~la$race + la$level)
+
+# RACE BY TYPE OF POSITION
+la.b2a <- ggplot(data = subset(df, !is.na(race)), aes(x = type)) + 
+  geom_bar(aes(fill = race), position = "fill") +
+  scale_y_continuous(labels = percent) +
+  scale_fill_manual(values = brewer.pal(9, "Blues")[c(8,6,4)]) +
+  labs(title = "Elected Offices by Race of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Race", x = "Type of Position", y = "Percent of Elected Officials") + 
+  geom_hline(yintercept = .626, col="black", linetype=2) + 
+  geom_hline(yintercept = .678, col="black", linetype=2) +
+  annotate("text", x = c(1,2,3,4,5,6), y = c(1.04, 1.04, 1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 651","n = 751", "n = 1848","n = 311","n = 152", "n = 867")) 
+
+xtabs(~la$type + la$race)
+
+# ..................................................................................................
+
+# PARTY BY LEVEL OF GOVERNMENT
+la.b3 <- ggplot(data = subset(la, !is.na(party)), aes(x = level)) + 
+  geom_bar(aes(fill = party), position = "fill") +
+  scale_fill_manual(values = c("#004eba", "#aa03ba", "#ba013c")) +
+  labs(title = "Elected Offices by Party of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Party", x = "Level of Government", y = "Percent of Elected Officials") +
+  annotate("text", x = c(1,2,3), y = c(1.04, 1.04, 1.04), 
+           label = c("n = 4348","n = 224", "n = 8"))  
+  
+
+# PARTY BY TYPE OF POSITION
+la.b3a <- ggplot(data = subset(la, !is.na(party)), aes(x = type)) + 
+  geom_bar(aes(fill = party), position = "fill") +
+  scale_fill_manual(values = c("#004eba", "#aa03ba", "#ba013c")) +
+  labs(title = "Elected Offices by Party of Elected Official, as of July 2018", 
+       subtitle = "Data from the State of Louisiana",
+       fill = "Party", x = "Type of Position", y = "Percent of Elected Officials") +
+  annotate("text", x = c(1,2,3,4,5,6), y = c(1.04, 1.04, 1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 651","n = 751", "n = 1848","n = 311","n = 152", "n = 867")) 
+
+# ..................................................................................................
+
+# LAW ENFORCEMENT
+
+# BY GENDER OF ELECTED OFFICIAL
+la.b1b <- ggplot(data = subset(enforce, !is.na(gender)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = gender), position = "fill") +
+  scale_fill_manual(values = c("#bf0549", "#059cbf")) +
+  labs(title = "Law Enforcement Offices by Gender of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Gender", x = "Position", y = "Percent of Elected Officials") +
+  scale_y_continuous(labels = percent) + 
+  geom_hline(yintercept = .5, col="black", linetype=2) +
+  annotate("text", x = c(1,2,3,4,5,6), y = c(1.04, 1.04, 1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 185","n = 387", "n = 362","n = 47","n = 42", "n = 64"))  
+
+prop.table(table(enforce$gender, enforce$genoffice), margin = 2)
+
+# BY RACE OF ELECTED OFFICIAL
+la.b2b <- ggplot(data = subset(df2, !is.na(race)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = race), position = "fill") +
+  scale_fill_manual(values = brewer.pal(9, "Blues")[c(8,6,4)]) +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Law Enforcement Offices by Race of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Race", x = "Position", y = "Percent of Elected Officials")  + 
+  geom_hline(yintercept = .626, col="black", linetype=2) + 
+  geom_hline(yintercept = .678, col="black", linetype=2) +
+  annotate("text", x = c(2,3,4,5,6,7), y = c(1.04, 1.04, 1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 185","n = 387", "n = 362","n = 47","n = 42", "n = 64")) 
+
+prop.table(table(enforce$race, enforce$genoffice), margin = 2)
+
+# BY PARTY OF ELECTED OFFICIAL
+la.b3b <- ggplot(data = subset(enforce, !is.na(party)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = party), position = "fill") +
+  scale_fill_manual(values = c("#004eba", "#aa03ba", "#ba013c")) +
+  labs(title = "Law Enforcement Offices by Party of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Party", x = "Position", y = "Percent of Elected Officials")+
+  annotate("text", x = c(1,2,3,4,5,6), y = c(1.04, 1.04, 1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 185","n = 387", "n = 362","n = 47","n = 42", "n = 64")) 
+
+prop.table(table(enforce$party, enforce$genoffice), margin = 2)
+
+# ..................................................................................................
+
+# LOCAL GOVERNMENT
+
+# BY GENDER OF ELECTED OFFICIAL
+la.b1c <- ggplot(data = subset(local, !is.na(gender)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = gender), position = "fill") +
+  scale_fill_manual(values = c("#bf0549", "#059cbf")) +
+  labs(title = "Local Government Offices by Gender of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Gender", x = "Position", y = "Percent of Elected Officials") +
+  scale_y_continuous(labels = percent) + 
+  geom_hline(yintercept = .5, col="black", linetype=2) +
+  annotate("text", x = c(1,2,3,4), y = c(1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 278","n = 665", "n = 21","n = 346")) 
+
+prop.table(table(local$gender, local$genoffice), margin = 2)
+
+# BY RACE OF ELECTED OFFICIAL
+la.b2c <- ggplot(data = subset(df3, !is.na(race)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = race), position = "fill") +
+  scale_fill_manual(values = brewer.pal(9, "Blues")[c(8,6,4)]) +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Local Government Offices by Race of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Race", x = "Position", y = "Percent of Elected Officials")  + 
+  geom_hline(yintercept = .626, col="black", linetype=2) + 
+  geom_hline(yintercept = .678, col="black", linetype=2) +
+  annotate("text", x = c(2,3,4,5), y = c(1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 278","n = 665", "n = 21","n = 346")) 
+
+prop.table(table(enforce$race, enforce$genoffice), margin = 2)
+
+# BY PARTY OF ELECTED OFFICIAL
+la.b3c <- ggplot(data = subset(local, !is.na(party)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = party), position = "fill") +
+  scale_fill_manual(values = c("#004eba", "#aa03ba", "#ba013c")) +
+  labs(title = "Local Government Offices by Party of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Party", x = "Position", y = "Percent of Elected Officials") +
+  annotate("text", x = c(1,2,3,4), y = c(1.04, 1.04, 1.04, 1.04), 
+           label = c("n = 278","n = 665", "n = 21","n = 346")) 
+
+prop.table(table(enforce$party, enforce$genoffice), margin = 2)
+
+# ..................................................................................................
+
+# URBAN RURAL DIVIDE IN MUNICIPAL GOVERNMENT
+
+# BY GENDER
+muni.b1 <- ggplot(data = subset(muni, !is.na(density)&!is.na(gender)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = gender), position = "fill") + facet_wrap(~density) +
+  scale_fill_manual(values = c("#bf0549", "#059cbf")) +
+  labs(title = "Differences in Urban & Rural Gender Representation in Local Government", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Gender", x = "Position", y = "Percent of Elected Officials") +
+  scale_y_continuous(labels = percent) + 
+  geom_hline(yintercept = .5, col="black", linetype=2)
+
+# BY RACE OF ELECTED OFFICIAL
+muni.b2 <- ggplot(data = subset(df4, !is.na(race)&!is.na(density)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = race), position = "fill") + facet_wrap(~density) +
+  scale_fill_manual(values = brewer.pal(9, "Blues")[c(8,6,4)]) +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Differences in Urban & Rural Racial Representation in Local Government", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Race", x = "Position", y = "Percent of Elected Officials") + 
+  geom_hline(yintercept = .626, col="black", linetype=2) + 
+  geom_hline(yintercept = .678, col="black", linetype=2)
+
+
+# BY PARTY OF ELECTED OFFICIAL
+muni.b3 <- ggplot(data = subset(muni, !is.na(party)&!is.na(density)), aes(x = genoffice)) + 
+  geom_bar(aes(fill = party), position = "fill") + facet_wrap(~density) +
+  scale_fill_manual(values = c("#004eba", "#aa03ba", "#ba013c")) +
+  labs(title = "Local Government Offices by Party of Elected Official", 
+       subtitle = "Data from the State of Louisiana, as of July 2018",
+       fill = "Party", x = "Position", y = "Percent of Elected Officials") 
+
+# ..................................................................................................
+
+# JUDGES
+
+j.b1 <- ggplot(data = subset(judges, !is.na(gender)), aes(x=office)) + 
+  geom_bar(aes(fill = gender), position = "fill")
+
+j.b2 <- ggplot(data = subset(judges, !is.na(race)), aes(x=office)) + 
+  geom_bar(aes(fill = race), position = "fill")
+
+j.b3 <- ggplot(data = subset(judges, !is.na(party)), aes(x=office)) + 
+  geom_bar(aes(fill = party), position = "fill")
+
+# ..................................................................................................
+
+save.image("viz.Rdata")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
